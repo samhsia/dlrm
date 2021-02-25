f86be6eda9c8b9f2610a2845d1c39b0a1cce3b14
Samuel Hsia <shsia@g.harvard.edu>
2021-02-25 13:53:58 -0500

    Fixed download script

--- download_data.sh
+++ download_data.sh
@@ -7,7 +7,7 @@ mkdir -p $DIR
 cd $DIR
 
 # Download Criteo Terabyte Dataset
-curl -O http://azuremlsampleexperiments.blob.core.windows.net/criteo/day_{`seq -s ‘,’ 0 23`}.gz
+curl -O http://azuremlsampleexperiments.blob.core.windows.net/criteo/day_{`seq -s , 0 23`}.gz
 
 # Unzip Criteo Terabyte Dataset
 for i in {0..23}
