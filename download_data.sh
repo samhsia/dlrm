7363ef4d199f66f7ad87268566057cc73e813e54
Samuel Hsia <shsia@g.harvard.edu>
2021-02-25 13:43:28 -0500

    Create download_data.sh

---
+++ download_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR="/data/terabyte"
+
+# Setup directories
+mkdir -p $DIR
+cd $DIR
+
+# Download Criteo Terabyte Dataset
+curl -O http://azuremlsampleexperiments.blob.core.windows.net/criteo/day_{`seq -s ‘,’ 0 23`}.gz
+
+# Unzip Criteo Terabyte Dataset
+for i in {0..23}
+do
+	gunzip day_$i.gz
+done
