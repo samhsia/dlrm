5c19c9e91967d98a98dcd22031cfd33916e3db61
Samuel Hsia <shsia@g.harvard.edu>
2021-02-27 17:49:45 -0500

    update docker mounting

--- docker/run-dev.sh
+++ docker/run-dev.sh
@@ -2,10 +2,10 @@
 
 docker run \
   --gpus all \
+  -v /home/schsia:/home/schsia:rw \
   -v /home/schsia/.cache:/home/schsia/.cache:ro \
   -v /group/brooks:/group/brooks:ro \
   -v /group/vlsiarch:/group/vlsiarch:rw \
-  -v /group/vlsiarch/ugupta/docker_home/:/home/ugupta:rw \
   -v /etc/passwd:/etc/passwd:ro \
   -v /data/:/data/:rw \
   -it --rm --user $(id -u) dlrm_terabyte:gpu  /bin/bash
